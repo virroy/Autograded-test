7c83310cef4c0048217ea2124daaade443ede825
virroy <virroydias@carmelcollegegoa.org>
2025-02-07 15:47:06 +0530

    Create python quiz.py

---
+++ python quiz.py
@@ -0,0 +1,5 @@
+3. Answer the questions by typing **a, b, c, or d** and pressing Enter.
+4. Your score will be automatically saved in a file called `results.txt`.
+5. Commit and push the `results.txt` file back to GitHub for submission.
+
+Good luck! 🚀
